a724d206136071afd3ac3deb7801e36cb0da3316
zwelinn <zewlinnmg@gmail.com>
2026-05-08 15:09:54 +0700

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -7,16 +7,16 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
-  socialProviders : {
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000", 
+  socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
-     github:{
-        clientId: process.env.GITHUB_CLIENT_ID!,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-     }
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }
   },
-  plugins :[nextCookies()],
-  baseUrl: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
+  plugins: [nextCookies()],
 });
